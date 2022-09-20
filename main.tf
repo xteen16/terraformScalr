c8850c76e54976d2e34b2e018b03a75c2ee7fef6
xteen16 <xteen@chequer.io>
2022-09-20 13:19:39 +0900

    add dummy code

---
+++ main.tf
@@ -0,0 +1,35 @@
+provider "aws" {
+  region = "eu-west-1"
+}
+
+data "aws_availability_zones" "available" {
+  state = "available"
+}
+
+variable "vpc_cidr" {
+  description = "CIDR for the VPC"
+  default     = "10.0.0.0/16"
+}
+
+variable "tag" {
+  default     = "terraformScalr"
+}
+
+resource "aws_vpc" "main" {
+  cidr_block           = var.vpc_cidr
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+  tags = {
+    Name = "${var.tag}-VPC"
+  }
+}
+
+resource "aws_subnet" "main_subnet" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.0.0/24"
+  availability_zone       = data.aws_availability_zones.available.names[0]
+  map_public_ip_on_launch = true
+  tags = {
+    Name = "${var.tag}-Subnet"
+  }
+}
